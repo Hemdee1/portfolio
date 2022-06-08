2a348625c68ec7577407c91529f91195053cf70d
hemdee <sanusi5110@gmail.com>
2022-06-08 02:57:29 +0100

    update location

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,9 +1,45 @@
 import "./contact.scss";
 import Animate from "../Animated-Letters/animated";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ImLocation2 } from "react-icons/im";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+  const locationRef = useRef();
+
+  const showLoc = () => {
+    locationRef.current.classList.toggle("fade");
+  };
+
+  const clearForm = () => {
+    form.current.name.value = "";
+    form.current.email.value = "";
+    form.current.message.value = "";
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_djqf1cv",
+        "template_yhk7o6o",
+        form.current,
+        "UI3QRB61PAUMjMx-1"
+      )
+      .then(
+        () => {
+          alert("Email sent succesfully");
+          clearForm();
+        },
+        () => {
+          alert("Error!!, Email is not sent");
+          clearForm();
+        }
+      );
+  };
+
   const [letterClass, setLetterClass] = useState("text-animate");
   const about = ["o", "n", "t", "a", "c", "t"];
   const about2 = ["M", "e"];
@@ -15,7 +51,7 @@ const Contact = () => {
   }, []);
 
   return (
-    <div className="page contact">
+    <div className="page contact" ref={locationRef}>
       <div className="cont">
         <h1>
           <span className="logo-letter">C</span>
@@ -26,13 +62,11 @@ const Contact = () => {
           I am interested in freelance opportunities - especially ambitious or
           large projects. Feel free to contact me below.
         </p>
-        <form>
-          <div>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Email" />
-          </div>
-          <input type="text" placeholder="Subject" />
-          <textarea placeholder="Message"></textarea>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" placeholder="Name" name="name" />
+          <input type="email" placeholder="Email" name="email" />
+
+          <textarea placeholder="Message" name="message"></textarea>
           <button className="btn">Send</button>
         </form>
       </div>
@@ -49,8 +83,9 @@ const Contact = () => {
         Osogbo, Osun State <br />
         Nigeria <br />
         09069010318 <br />{" "}
-        <a href="mailto:Sanusi5110@gmail.com">Sanusi5110@gmail.com</a>
+        <a href="mailto:Sanusi5110@gmail.com">Muhyideen5110@gmail.com</a>
       </div>
+      <ImLocation2 className="loc-btn" onClick={showLoc} />
     </div>
   );
 };
